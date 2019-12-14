48ae74399b2dbb868d80cbd946c9490e4854379e
Jerry Chang <therealjerrycan@gmail.com>
2019-12-14 12:58:28 -0800

    Update to include comments about the various presets, plugins and add info link

--- packages/babel/babel.config.js
+++ packages/babel/babel.config.js
@@ -1,26 +1,51 @@
 const commonPresets = [
     '@babel/preset-env',
+
+    // React support
     '@babel/preset-react',
+
+    // TS support
     '@babel/preset-typecript',
 ];
 
 const commonPlugins = [
-    // Support decorators
+    /**
+    *   link: https://babeljs.io/docs/en/babel-plugin-proposal-decorators 
+    *
+    *   Support decorators
+    */
     '@babel/plugin-proposal-decorators',
 
-    // Support decorators
+    /**
+    *   link: https://babeljs.io/docs/en/babel-plugin-proposal-function-bind
+    *
+    *   Support func bind syntax obj::func -> func.bind(obj)
+    */
     '@babel/plugin-proposal-function-bind',
 
-    // Support arrow functions in classes
+    /**
+    *   link: https://babeljs.io/docs/en/babel-plugin-proposal-class-properties
+    *
+    *   Support arrow functions in class 
+    */
     '@babel/plugin-proposal-class-properties'
     ['@babel/plugin-transform-classes', {'loose': true}]
 
-    // Support optional chaining -> a?.b
+    /**
+    *   link: https://babeljs.io/docs/en/babel-plugin-syntax-optional-chaining 
+    *
+    *   Support optional chaining -> a?.b
+    */
     '@babel/plugin-proposal-optional-chaining'
 
     '@babel/plugin-proposal-export-default-from',
     ['@babel/plugin-proposal-export-default-from', { 'legacy': true }]
 
+    /**
+    *   link: https://babeljs.io/docs/en/babel-plugin-transform-proto-to-assign 
+    *
+    *   Support transform __proto__ to assign -> bar.__proto__ = x
+    */
     '@babel/plugin-transform-proto-to-assign',
     ['@babel/plugin-transform-runtime']
 ];
