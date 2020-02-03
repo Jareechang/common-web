1a4718f8eca5829bfa0cce1ed373b58692ceb350
Jerry Chang <therealjerrycan@gmail.com>
2020-02-02 19:53:33 -0800

    init common jest configuration

---
+++ packages/jest/jest.config.js
@@ -0,0 +1,44 @@
+const definedRoot = process.env.COMMON_JEST_ROOT || '';
+const supportedFileExtensions = process.env.COMMON_JEST_FILE_EXTENSIONS || '';
+const isProduction = process.env.NODE_ENV === 'production';
+const ignorePatterns = process.env.COMMON_JEST_IGNORE_PATTERNS || '';
+
+function stringToArrayConfig(config) {
+    if (typeof config === 'string') return config.split(',');
+
+    return [];
+}
+
+module.exports = {
+    verbose: !isProduction,
+    roots: definedRoot || [
+        'src'
+    ],
+
+    testMatch: [
+        "**/__tests__/**/*.[jt]s?(x)",
+        "**/?(*.)+(spec|test).[jt]s?(x)"
+    ],
+
+    transform: {
+      "^.+\\.jsx?$": "babel-jest",
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+
+    moduleFileExtensions: [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json",
+        "node"
+    ].concat(stringToArrayConfig(supportedFileExtensions)),
+
+    coveragePathIgnorePatterns: [
+        'node_modules'
+    ].concat(stringToArrayConf(ignorePatterns)),
+
+    transformIgnorePatterns: [
+        'node_modules'
+    ].concat(stringToArrayConf(ignorePatterns)),
+};
